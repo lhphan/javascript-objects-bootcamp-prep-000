66a2975a2e90edee52b20470fe754ec81f7e9d39
Lien Phan <whyofcourse@gmail.com>
2017-09-10 22:46:51 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,6 @@
 var playlist = {"The Postal Service" : "Brand New Colony"}
 
 function updatePlaylist(playlist, artistName, songTitle){
-  return Object.assign({}, playlist, {Object[artistName]: songTitle})
+  playlist[artistName] = songTitle
+  return playlist
 }
